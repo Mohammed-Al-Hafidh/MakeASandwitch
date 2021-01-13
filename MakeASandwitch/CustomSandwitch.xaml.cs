16fe91964f4c4105fe6ea16500ee01f7f87e3581
Mohammed Al-Hafidh <66840344+Mohammed-Al-Hafidh@users.noreply.github.com>
2021-01-13 09:36:47 -0500

    Add project files.

---
+++ MakeASandwitch/CustomSandwitch.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MakeASandwitch
+{
+    /// <summary>
+    /// Interaction logic for CustomSandwitch.xaml
+    /// </summary>
+    public partial class CustomSandwitch : Window
+    {
+        public event Action<string, string, string> BackValues;
+        public CustomSandwitch()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string bread = combBread.Text;
+            string vegetables = "";
+            if (chkbLettuce.IsChecked == true)
+            {
+                vegetables += chkbLettuce.Content.ToString() + " ";
+            }
+            if (chkbTomatos.IsChecked == true)
+            {
+                vegetables += chkbTomatos.Content.ToString() + " ";
+            }
+            if (chkbCucumbers.IsChecked == true)
+            {
+                vegetables += chkbCucumbers.Content.ToString() + " ";
+            }
+            string meats = "";
+            if (rdbChicken.IsChecked == true)
+                meats = rdbChicken.Content.ToString();
+            if (rdbTurki.IsChecked == true)
+                meats = rdbTurki.Content.ToString();
+            if (rdbTofu.IsChecked == true)
+                meats = rdbTofu.Content.ToString();
+            BackValues?.Invoke(bread, vegetables, meats);
+            DialogResult = true;
+
+            
+        }
+    }
+}
---
+++ MakeASandwitch/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MakeASandwitch
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMakeSandwitch_Click(object sender, RoutedEventArgs e)
+        {
+            string bread = "";
+            string veggis = "";
+            string meat="";
+
+            CustomSandwitch customSandwitch = new CustomSandwitch();
+            customSandwitch.Owner = this;
+
+            customSandwitch.BackValues += (a, b, c) => { bread = a; veggis = b; meat = c; };
+            bool? result = customSandwitch.ShowDialog();
+            if (result == true)
+            {
+                lblBread.Content = bread;
+                lblVeggis.Content = veggis;
+                lblMeat.Content = meat;
+            }
+            else if (result == false)
+            {
+                MessageBox.Show("Request canceled","Cancel",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+
+        }
+    }
+}
